2712dd3b487a5735ad7bbd11171ebfa30d7fc092
mohlade <100697842+mohlade@users.noreply.github.com>
2022-06-16 18:57:26 +0300

    added components

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import face from './face.png';
 const Logo = () =>{
     return (
         <div className='ma4 mt0'>
-         <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
+         <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
            <div className="Tilt-inner"> <img alt = "logo" src ={face} ></img></div>
          </Tilt>
 
